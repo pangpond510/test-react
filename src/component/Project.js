febc1829382f6cf328d640d1f0399f7b112ae595
Norawit Hempornwisarn <pangpond_32552@hotmail.com>
2017-06-16 14:06:08 +0700

    add css of Project component

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Data from './data'
 import ProjEntry from './ProjEntry';
+import './css-component/Project.css';
 
 class Project extends Component {
   render() {
